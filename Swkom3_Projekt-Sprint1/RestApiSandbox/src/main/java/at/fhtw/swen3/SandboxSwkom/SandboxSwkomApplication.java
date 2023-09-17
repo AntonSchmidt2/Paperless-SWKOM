200646eccbc5f3a0bac3a9824589c5d7fa8be568
Anton Schmidt <schmidt.anton2@gmail.com>
2023-09-17 22:31:17 +0200

    added Openapi dependency for code generation within Intellij (plugin) Added provided swagger definition

--- Swkom3_Projekt-Sprint1/RestApiSandbox/src/main/java/at/fhtw/swen3/SandboxSwkom/SandboxSwkomApplication.java
+++ Swkom3_Projekt-Sprint1/RestApiSandbox/src/main/java/at/fhtw/swen3/SandboxSwkom/SandboxSwkomApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 // start the project and go to url http://localhost:8082/Paperless/main to see all document entries
+// for API documentation go to http://localhost:8082/swagger-ui/index.html#/
 
 @SpringBootApplication
 public class SandboxSwkomApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SandboxSwkomApplication.class, args);
 	}
