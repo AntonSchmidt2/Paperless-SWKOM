cd85bbdba7422d97ba67e655eb1ac0d02f9ede29
Anton Schmidt <schmidt.anton2@gmail.com>
2023-12-29 18:49:35 +0100

    implemented document upload to MinIO

--- src/main/java/org/openapitools/serviceLayer/services/DocumentServiceImpl.java
+++ src/main/java/org/openapitools/serviceLayer/services/DocumentServiceImpl.java
@@ -2,6 +2,7 @@ package org.openapitools.serviceLayer.services;
 
 import io.minio.BucketExistsArgs;
 import io.minio.MakeBucketArgs;
+import io.minio.errors.*;
 import org.openapitools.OpenApiGeneratorApplication;
 import org.openapitools.persistence.entities.DocumentEntity;
 import org.openapitools.persistence.repositories.DocumentRepository;
@@ -13,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import io.minio.MinioClient;
-import io.minio.errors.MinioException;
 import io.minio.PutObjectArgs;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -35,50 +35,73 @@ public class DocumentServiceImpl {
 
     public DocumentServiceImpl(DocumentRepository documentRepository, MinioClient minioClient, DocumentMapper documentMapper, RabbitMQSender rabbitMQSender) {
         this.documentRepository = documentRepository;
-        this.minioClient = minioClient;
+        this.minioClient = MinioClient.builder()
+                .endpoint("http://host.docker.internal:9000")
+                .credentials("paperless", "paperless")
+                .build();
         this.documentMapper = documentMapper;
         this.rabbitMQSender = rabbitMQSender;
     }
 
     @Transactional
     public void uploadDocument(DocumentDTO documentDTO) {
-        logger.info("uploadDocument started");
+        try {
+            logger.info("uploadDocument started");
 
-        documentDTO.setCreated(OffsetDateTime.now());
-        documentDTO.setAdded(OffsetDateTime.now());
-        documentDTO.setModified(OffsetDateTime.now());
-        documentDTO.content("");
-        documentDTO.setAdded(OffsetDateTime.now());
+            documentDTO.setCreated(OffsetDateTime.now());
+            documentDTO.setAdded(OffsetDateTime.now());
+            documentDTO.setModified(OffsetDateTime.now());
+            documentDTO.content("");
+            documentDTO.setAdded(OffsetDateTime.now());
 
-        DocumentEntity documentToBeSaved = documentMapper.dtoToEntity(documentDTO);
-        documentToBeSaved.setStorageType("pdf");
-        documentToBeSaved.setMimeType("pdf");
+            DocumentEntity documentToBeSaved = documentMapper.dtoToEntity(documentDTO);
+            documentToBeSaved.setStorageType("pdf");
+            documentToBeSaved.setMimeType("pdf");
 
-        logger.info("Saving document to the repository");
-        documentRepository.save(documentToBeSaved);
-        logger.info("Document saved to the repository");
+            logger.info("Saving document to the repository");
+            documentRepository.save(documentToBeSaved);
+            logger.info("Document saved to the repository");
 
-        // Upload file to MinIO
-        // MultipartFile multipartFile = documentDTO.getDocument();
-        // String bucketName = "paperless-bucket"; // replace with your bucket name
+            //Upload file to MinIO
+            MultipartFile multipartFile = documentDTO.getDocument();
+            String bucketName = "paperless-bucket"; // replace with your bucket name
 
-        // if (!minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build())) {
-        //     minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
-        // }
+            if (!minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build())) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+            }
 
-        // try {
-        //     minioClient.putObject(
-        //             PutObjectArgs.builder()
-        //                     .bucket(bucketName)
-        //                     .object(multipartFile.getOriginalFilename())
-        //                     .stream(multipartFile.getInputStream(), multipartFile.getSize(), -1)
-        //                     .contentType(multipartFile.getContentType())
-        //                     .build());
-        //     logger.info("Document uploaded to MinIO");
-        // } catch (MinioException | IOException e) {
-        //     logger.error("Error occurred while uploading file to MinIO", e);
-        // } catch (NoSuchAlgorithmException | InvalidKeyException e) {
-        //     logger.error("Error occurred", e);
-        // }
+            try {
+                minioClient.putObject(
+                        PutObjectArgs.builder()
+                                .bucket(bucketName)
+                                .object(multipartFile.getOriginalFilename())
+                                .stream(multipartFile.getInputStream(), multipartFile.getSize(), -1)
+                                .contentType(multipartFile.getContentType())
+                                .build());
+                logger.info("Document uploaded to MinIO");
+            } catch (MinioException | IOException e) {
+                logger.error("Error occurred while uploading file to MinIO", e);
+            } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+                logger.error("Error occurred", e);
+            }
+        } catch (ErrorResponseException e) {
+            throw new RuntimeException(e);
+        } catch (InsufficientDataException e) {
+            throw new RuntimeException(e);
+        } catch (InternalException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeyException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidResponseException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (ServerException e) {
+            throw new RuntimeException(e);
+        } catch (XmlParserException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
