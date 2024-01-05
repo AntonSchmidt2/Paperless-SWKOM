0d570dda81914c99792fcda68175c08b6add2859
Daniel Čerňanský <daniel.cernansky88@gmail.com>
2024-01-05 20:03:09 +0100

    added dependency/healthcheck on rabbitmq for tesseract image + fixed ocr service to wait for queue creation

--- Tesseract/src/main/java/org/openapitools/Services/OcrService.java
+++ Tesseract/src/main/java/org/openapitools/Services/OcrService.java
@@ -7,6 +7,7 @@ import io.minio.errors.MinioException;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
@@ -21,9 +22,11 @@ public class OcrService {
     private final ITesseract tesseract;
     private final RabbitTemplate rabbitTemplate;
     private final MinioClient minioClient;
+    private final AmqpAdmin amqpAdmin;
 
-    public OcrService(RabbitTemplate rabbitTemplate) {
+    public OcrService(RabbitTemplate rabbitTemplate, AmqpAdmin amqpAdmin) {
         this.rabbitTemplate = rabbitTemplate;
+        this.amqpAdmin = amqpAdmin;
         //minIO client has to refer to docker address
         this.minioClient = MinioClient.builder()
                 .endpoint("http://host.docker.internal:9000")
@@ -31,18 +34,18 @@ public class OcrService {
                 .build();
         this.tesseract = new Tesseract();
         this.tesseract.setLanguage("eng");
+        waitForQueueCreation("OCR_DOCUMENT_IN");
     }
 
     @RabbitListener(queues = "OCR_DOCUMENT_IN") // Replace with the actual queue name
-    public void processMessage() {
+    public void processMessage(String message) {
         try {
-            String fileName = "Test.pdf";
-            InputStream file = getFileFromMinio(fileName);
+            InputStream file = getFileFromMinio(message);
             String text = extractText((MultipartFile) file);
             System.out.println("Extracted Text: " + text);
 
             // Send the extracted text back to RabbitMQ (publish to a new queue or the same queue)
-            rabbitTemplate.convertAndSend("extractedText-queue", text);
+            //rabbitTemplate.convertAndSend("extractedText-queue", text);
 
         } catch (IOException | TesseractException e) {
             e.printStackTrace();
@@ -77,5 +80,16 @@ public class OcrService {
         File tempFile = File.createTempFile("temp", null);
         multipartFile.transferTo(tempFile);
         return tempFile;
+    }
+
+    private void waitForQueueCreation(String queueName) {
+        // Add a loop to wait for the queue to be declared
+        while (amqpAdmin.getQueueProperties(queueName) == null) {
+            try {
+                Thread.sleep(5000);  // Sleep for 1 second before checking again
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
